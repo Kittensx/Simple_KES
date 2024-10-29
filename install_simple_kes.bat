a61a9e37fcb99111f2ec494ce62693d11ecd4111
Kittensx <photographer.ksu30@gmail.com>
2024-10-29 10:23:05 -0400

    Update install_simple_kes.bat
    Signed-off-by: Kittensx <photographer.ksu30@gmail.com>

--- install_simple_kes.bat
+++ install_simple_kes.bat
@@ -3,19 +3,20 @@
 python --version >nul 2>&1
 IF %ERRORLEVEL% NEQ 0 (
     echo Python is not installed or not added to PATH.
+    pause
     exit /b 1
-)
+	)
 
 :: Run the Python script
-echo Running install_requirements.py...
-python install_requirements.py
+echo Running install_simple_kes.py...
+python install_simple_kes.py
 
 :: Check if the script executed successfully
 IF %ERRORLEVEL% EQU 0 (
     echo Requirements installation completed successfully.
 ) ELSE (
     echo An error occurred while installing requirements.
-    exit /b 1
 )
 
+:: Pause at the end to keep the window open
 pause
